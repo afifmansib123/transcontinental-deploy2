5ef43a4092e7da8ee58eea6d4406a7958f998771
Afif-Mac <afifmansib123@gmail.com>
2023-07-17 14:45:06 +0700

    nice design

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home({ products, featuredProducts }) {
   const endIndex = startIndex + pageSize;
   const productsToShow = products.slice(startIndex, endIndex);
 
+
   //saving coebase before shop
 
   const { state, dispatch } = useContext(Store);
@@ -56,13 +57,13 @@ export default function Home({ products, featuredProducts }) {
         <h1 className='flex justify-center' style={{ fontSize: 20, color: "white", whiteSpace: "nowrap" }}>IMPORT EXPORT & TRADE</h1>
       </div>
 
-      <Carousel showThumbs={false} autoPlay interval={1500} className="full-screen object-fill" style={{ width: '100%', height: '100%' }}>
+      <Carousel showThumbs={false} autoPlay interval={1700} className="full-screen object-fill" style={{ width: '100%', height: '100%' }}>
         {featuredProducts.map((product, index) => (
           <div key={product._id}>
             <Link href={`#`} passHref className="flex">
               <img src={product.banner} alt={product.name} className="carousel-image" />
-              
-              {index === 1 && <div className="overlay-text" style={{color:"#072644",  fontSize: 30, top:200}}> IMPORT EXPORT & TRADE WORLDWIDE <br/>Meet & Close deals with <br></br>Hundreds of Vendors in global Market</div>}
+
+              {index === 1 && <div className="overlay-text" style={{ color: "#072644", fontSize: 30, top: 200 }}>First Trading Hub Of South-East Asia<br />Import Export Trade And Meet<br></br>Hundreds of Vendors Worldwide</div>}
             </Link>
           </div>
         ))}
@@ -72,22 +73,60 @@ export default function Home({ products, featuredProducts }) {
       <br />
 
       <Link href="search?query=">
-        <h1 className='flex justify-center' style={{ fontSize: 25, color: "#10539D", whiteSpace: "nowrap" }}>SEARCH WITH EASE<Image src={`/images/y.png`} alt="hello" height={10} width={40} className='ml-3'/></h1>
+        <h1 className='flex justify-center' style={{ fontSize: 25, color: "#10539D", whiteSpace: "nowrap" }}>SEARCH WITH EASE<Image src={`/images/y.png`} alt="hello" height={10} width={40} className='ml-3' /></h1>
+        </Link>
         <br />
-         <div className="full-spanning-row" style={{ height: 80 }}> 
-          <button className="full-width-button" style={{ fontSize:25, color:"Black", backgroundColor: "#2567B5" }}>Catagories</button>
-          <button className="full-width-button" style={{ fontSize:25, color:"Black",backgroundColor: "#6790C0" }}>Brands</button>
-          <button className="full-width-button" style={{ fontSize:25, color:"Black",backgroundColor: "#A8C2DF" }}>Prices</button>
-          <button className="full-width-button" style={{ fontSize:25, color:"Black",backgroundColor: "#CEDDEE" }}>Featured</button>
+        <div className="full-spanning-row" style={{ height: 80  }}>
+        <button className="full-width-button" style={{ fontSize: 25, color: "Black", backgroundColor: "#2567B5" }}><Link href="search?query=" >Catagories</Link></button>
+         <button className="full-width-button" style={{ fontSize: 25, color: "Black", backgroundColor: "#6790C0" }}><Link href="search?query="> Brands</Link></button>
+         <button className="full-width-button" style={{ fontSize: 25, color: "Black", backgroundColor: "#A8C2DF" }}><Link href="search?query="> Prices</Link></button>
+        <button className="full-width-button" style={{ fontSize: 25, color: "Black", backgroundColor: "#CEDDEE" }}><Link href="search?query="> Featured</Link></button>
         </div>
-      </Link>
+      
+
+
+        <div className='grid grid-cols-4 gap-0 md:grid-cols-4 lg:grid-cols-4 items-center'>
+        <div class="dropdown">
+          <Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image>
+          <div class="dropdown-content">
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+          </div>
+        </div>
+
+        <div class="dropdown">
+          <Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image>
+          <div class="dropdown-content">
+          <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image></a>
+          </div>
+        </div>
+
+        <div class="dropdown">
+          <Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image>
+          <div class="dropdown-content">
+          <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={100}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={100}></Image></a>
+          </div>
+        </div>
+
+        <div class="dropdown">
+          <Image class="dropbtn" src={`/images/background1.png`} height={250} width={250}></Image>
+          <div class="dropdown-content">
+          <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+            <a href="#"><Image class="dropbtn" src={`/images/background1.png`} height={250} width={220}></Image></a>
+          </div>
+        </div>
+
+      </div>
 
 
 
-
-
-
-      <h2 className="h2 my-4 flex justify-center" style={{color: "#0C3B7E", fontSize:30}}>Latest Arrivals</h2>
+      <h2 className="h2 my-4 flex justify-center" style={{ color: "#0C3B7E", fontSize: 30 }}>Latest Arrivals</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {productsToShow.map((product) => (
           <ProductItem
@@ -99,28 +138,33 @@ export default function Home({ products, featuredProducts }) {
       </div>
 
 
-      <div style={{ position: "relative"}}>
-      <Image src={`/images/background4.gif`} width={100} height={100} className="full-screen"></Image>
-      <div className="image-overlay mb-0 mt-0">
-        <p className='flex justify-center mb-0 mt-0' style = {{fontSize:21, color: "#1C448C"}}>WHO ARE WE?</p>
-    <p className="image-overlay-text" style={{color: "#15336A"}}>TC is the leading import-export trade hub in Bangladesh, with HQ in both Thailand. Our platform facilitates import-export operations across diverse industries personalizing virtual storefronts,robust logistics networks, and dedicated support in smooth transactions. Committed to sustainability,trade and ethical sourcing.<Link style={{color:"#D14343"}} href={`/aboutus`}>...see more</Link></p>
-  </div>
+      
+
+
+      <div style={{ position: "relative" }}>
+        <Image src={`/images/background4.gif`} width={100} height={100} className="full-screen"></Image>
+        <div className="image-overlay mb-0 mt-0">
+          <p className='flex justify-center mb-0 mt-0' style={{ fontSize: 21, color: "#1C448C" }}>WHO ARE WE?</p>
+          <p className="image-overlay-text" style={{ color: "#15336A" }}>TC is the leading import-export trade hub in Bangladesh, with HQ in both Thailand. Our platform facilitates import-export operations across diverse industries personalizing virtual storefronts,robust logistics networks, and dedicated support in smooth transactions. Committed to sustainability,trade and ethical sourcing.<Link style={{ color: "#D14343" }} href={`/aboutus`}>...see more</Link></p>
+        </div>
       </div>
 
 
 
-      <h2 className="h2 my-4 flex justify-center" style={{color: "#0C3B7E", fontSize:30}}>Latest Arrivals</h2>
+
+
+      <h2 className="h2 my-4 flex justify-center" style={{ color: "#0C3B7E", fontSize: 30 }}>Latest Arrivals</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {productsToShow.filter((product)=>product.category === "excavator")
-        .slice(0,4)
-        .map((product) => (
-          <ProductItem
-            product={product}
-            key={product.slug}
-            addToCartHandler={addToCartHandler}
-          ></ProductItem>
-        ))}
-        
+        {productsToShow.filter((product) => product.category === "excavator")
+          .slice(0, 4)
+          .map((product) => (
+            <ProductItem
+              product={product}
+              key={product.slug}
+              addToCartHandler={addToCartHandler}
+            ></ProductItem>
+          ))}
+
       </div>
 
 
