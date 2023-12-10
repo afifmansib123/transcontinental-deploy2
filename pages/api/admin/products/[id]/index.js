6374237c51fea157c6d6163ad32e289d1252626b
Afif-Mac <afifmansib123@gmail.com>
2023-12-10 18:53:50 +0700

    delete working

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -53,12 +53,14 @@ const deleteHandler = async (req, res) => {
   await db.connect();
   const product = await Product.findById(req.query.id);
   if (product) {
-    await product.remove();
-    await db.disconnect();
+    await Product.deleteOne({_id : req.query.id})
+    //await product.remove()
+    //await db.disconnect();
     res.send({ message: 'Product deleted successfully' });
   } else {
     await db.disconnect();
     res.status(404).send({ message: 'Product not found' });
   }
-};
+}
+
 export default handler;
